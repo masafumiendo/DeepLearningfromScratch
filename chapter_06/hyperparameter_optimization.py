3ebb7874d40170b8ef46e25a5ca08dad3c850ae6
Masafumi Endo <endoma@oregonstate.edu>
2020-02-02 23:40:00 -0800

    fix bug

--- chapter_06/hyperparameter_optimization.py
+++ chapter_06/hyperparameter_optimization.py
@@ -16,7 +16,7 @@ validation_rate = 0.20
 validation_num = int(x_train.shape[0] * validation_rate)
 x_train, t_train = shuffle_dataset(x_train, t_train)
 x_val = x_train[:validation_num]
-t_val = x_train[:validation_num]
+t_val = t_train[:validation_num]
 x_train = x_train[validation_num:]
 t_train = t_train[validation_num:]
 
